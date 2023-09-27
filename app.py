688cf265d3cd24d28ca81c792270424c3702a015
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-09-27 23:08:56 +0530

    linked with plot.py and displayed the stocks data

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 from JSONToDF import JSONToDF
+import matplotlib
+import plot.py
 
 
+API_KEY = "<well u knw, its encrypted>"
 
-API_KEY = "ZRRO9VW6CVON92DZ"
 
-# App title
 st.title('Stonks')
 
 symbol = st.text_input('Enter your stonk: eg: APPL for Apple Stonk')
@@ -18,7 +19,7 @@ if st.button('Predict'):
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}.BSE&outputsize=full&apikey={API_KEY}"
     response = requests.get(url)
     json_data = response.json()
-    print(json_data)
+    #print(json_data)
 
 
 
@@ -27,15 +28,15 @@ if st.button('Predict'):
 
 
     #Print(data)
-    #st.write(data)
+    st.write(data)
     #print(data)
-    # if data['status'] == 'OK':
-    #     result  = data["results"][0]
-    #     st.write(f"**{symbol}** Stock Price Prediction:")
-    #     st.write(f"Open: ${result['o']}")
-    #     st.write(f"Close: ${result['c']}")
-    #     st.write(f"High: ${result['h']}")
-    #     st.write(f"Low: ${result['l']}")
-    # else:
-    #     st.write("Invalid symbol or an error occurred.")
+    if data['status'] == 'OK':
+        result  = data["results"][0]
+        st.write(f"**{symbol}** Stock Price Prediction:")
+        st.write(f"Open: ${result['o']}")
+        st.write(f"Close: ${result['c']}")
+        st.write(f"High: ${result['h']}")
+        st.write(f"Low: ${result['l']}")
+    else:
+        st.write("Invalid symbol or an error occurred.")
 
