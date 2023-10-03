1888f270eb2e4d6cca6bac41a2a1f24b42180e02
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-10-03 21:42:12 +0530

    Update back.js

--- back.js
+++ back.js
@@ -14,6 +14,17 @@ const app = express();
 // app.use('/api/stocks', require('./routes/stockRoutes'));
 
 const PORT = process.env.PORT || 3000;
+mongoose.connect('mongodb://localhost/your-database-name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB', err);
+});
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
