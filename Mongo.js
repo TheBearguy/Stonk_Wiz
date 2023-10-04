dfe60dc96651365440c73cfb5e7ad09515705e20
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-10-04 22:27:20 +0530

    Update Mongo.js

--- Mongo.js
+++ Mongo.js
@@ -61,3 +61,7 @@ app.listen(port, () => {
 node app.js
 ```
 
+This code creates a simple Express.js web server that connects to MongoDB and provides two routes: `/` for the homepage and `/data` to retrieve data from MongoDB. You can expand upon this code to build your website and implement CRUD operations on your MongoDB database as needed.
+
+Remember to keep sensitive information like database credentials in a secure configuration file or environment variables and not hardcode them in your code for production use.
+  
